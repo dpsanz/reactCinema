e0295b906b54849c7b98bb0ec57b1b2fb399287b
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-06 12:58:54 -0300

    1

---
+++ danielCinemas/src/componentes/Header/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <header>
+            <span> Olá, visitante!</span>
+            <h1> Bem-vindo ao Cine Tutopirulin</h1>
+        </header>
+    )
+}
+
+export default Header
